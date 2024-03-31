f375a3c4124e53aaa1bb828b1b367d0666fede6b
vasiliiok <starvasyaa@gmail.com>
2024-03-31 18:14:49 +0300

    commit

--- 2.cpp
+++ 2.cpp
@@ -13,46 +13,58 @@ public:
     }
 
 public:
-    static bool inversion(bool value_1) {
+    static bool inversion(unsigned int value_1) {
         return !value_1;
     }
 
-    static bool conjunction(bool value_1, bool value_2) {
+    static bool conjunction(unsigned int value_1, unsigned int value_2) {
         return value_1 * value_2;
     }
 
-    static bool disjunction(bool value_1, bool value_2) {
+    static bool disjunction(unsigned int value_1, unsigned int value_2) {
         return value_1 + value_2;
     }
 
-    static bool implication(bool value_1, bool value_2) {
+    static bool implication(unsigned int value_1, unsigned int value_2) {
         return disjunction(conjunction(value_1, value_2),
                            inversion(value_1));
     }
 
-    static bool coimplication(bool value_1, bool value_2) {
+    static bool coimplication(unsigned int value_1, unsigned int value_2) {
         return inversion(implication(value_1, value_2));
     }
 
-    static bool exclusiveOr(bool value_1, bool value_2) {
+    static bool exclusiveOr(unsigned int value_1, unsigned int value_2) {
         return disjunction(conjunction(value_1, inversion(value_2)),
                            conjunction(inversion(value_1), value_2));
     }
 
-    static bool equals(bool value_1, bool value_2) {
+    static bool equals(unsigned int value_1, unsigned int value_2) {
         return inversion(exclusiveOr(value_1, value_2));
     }
 
-    static bool pierceArrow(bool value_1, bool value_2) {
+    static bool pierceArrow(unsigned int value_1, unsigned int value_2) {
         return inversion(disjunction(value_1, value_2));
     }
 
-    static bool schaefferStroke(bool value_1, bool value_2) {
+    static bool schaefferStroke(unsigned int value_1, unsigned int value_2) {
         return inversion(conjunction(value_1, value_2));
+    }
+
+public:
+    unsigned char getBit(unsigned int index) const {
+        return value_ & (1u << index);
+    }
+
+    void write(char *pointer) {
+        char *temp_pointer = pointer;
+        for (int i = 0; i < sizeof(unsigned int) * 8; ++i) {
+            *(temp_pointer++) = getBit(i);
+        }
+        *temp_pointer = 0;
     }
 };
 
 int main() {
-    LogicalValuesArray obj(0);
-    std::cout << !456;
+
 }

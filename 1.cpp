573ad4abbd64c02291018c2ab772d104ae59b00a
vasiliiok <starvasyaa@gmail.com>
2024-03-31 18:34:47 +0300

    commit

--- 1.cpp
+++ 1.cpp
@@ -3,15 +3,6 @@
 
 class encoder {
 private:
-    static void swap(int i, int j, unsigned char *const &arr) {
-        unsigned char temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-private:
-    // Как без static получить доступ к s_block_ из struct ByteGenerator??
-    static unsigned char s_block_[256];
     unsigned char *key_ = nullptr;
     size_t key_size_ = 0;
 
@@ -32,6 +23,18 @@ private:
         }
     }
 
+    void keyMutator (unsigned char const *key, size_t size) {
+        if(key_size_ != size) {
+            clearKeyMemory();
+            allocateKeyMemory(size);
+        }
+        copyKey(key);
+        init();
+    }
+
+private:
+    unsigned char s_block_[256]{};
+
     void init() {
         for (int i = 0; i < 256; ++i) {
             s_block_[i] = i;
@@ -40,30 +43,20 @@ private:
         int j = 0;
         for (int i = 0; i < 256; ++i) {
             j = (j + s_block_[i] + key_[i % key_size_]) % 256;
-            swap(i, j, s_block_);
+            std::swap(s_block_[i], s_block_[j]);
         }
     }
 
-    struct ByteGenerator {
-        int x = 0, y = 0;
-        unsigned char s_block[256] = {0};
+    int x = 0, y = 0;
+    unsigned char gen()
+    {
+        x = (x + 1) % 256;
+        y = (y + s_block_[x]) % 256;
 
-        ByteGenerator() {
-            for (int i = 0; i < 256; ++i) {
-                s_block[i] = s_block_[i];
-            }
-        }
+        std::swap(s_block_[x], s_block_[y]);
 
-        unsigned char gen()
-        {
-            x = (x + 1) % 256;
-            y = (y + s_block[x]) % 256;
-
-            swap(x, y, s_block);
-
-            return s_block[(s_block[x] + s_block[y]) % 256];
-        }
-    };
+        return s_block_[(s_block_[x] + s_block_[y]) % 256];
+    }
 
 public:
     encoder(unsigned char const *key, size_t size) {
@@ -89,19 +82,10 @@ public:
         return *this;
     }
 
-    void keyMutator (unsigned char const *key, size_t size) {
-        if(key_size_ != size) {
-            clearKeyMemory();
-            allocateKeyMemory(size);
-        }
-        copyKey(key);
-        init();
-    }
-
+public:
     void encode(char const *input_path, char const *output_path, bool flag) {
         std::ifstream input_file(input_path, std::ios_base::binary);
         std::ofstream output_file(output_path, std::ios_base::binary);
-        ByteGenerator generator;
 
         if (input_file.is_open() && output_file.is_open())
         {
@@ -109,7 +93,7 @@ public:
             while (true) {
                 ch = input_file.get();
                 if (input_file.eof()) break;
-                output_file.put(ch ^ generator.gen());
+                output_file.put(ch ^ gen());
             }
         } else {
             std::cout << "Cannot open file!!";
